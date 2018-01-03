be4d8499b3d63e384bf819f5d99911bea1973d42
leafxt <leafxt@163.com>
2018-01-03 18:28:40 +0800

    update

---
+++ src/main/java/net/windsoft/application/controller/TestController.java
@@ -0,0 +1,12 @@
+package net.windsoft.application.controller;
+
+import org.springframework.stereotype.Controller;
+
+import net.windsoft.webdevil.web.controller.BaseController;
+
+@Controller
+public class TestController extends BaseController {
+	
+	
+	
+}
