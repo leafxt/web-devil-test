c291ff894432229838d3511db1b767f77d6c11e1
leafxt <leafxt@163.com>
2018-01-04 00:02:51 +0800

    update

---
+++ src/main/java/net/windsoft/application/controller/TestController.java
@@ -0,0 +1,24 @@
+package net.windsoft.application.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.windsoft.webdevil.core.web.controller.BaseController;
+
+@Controller
+@RequestMapping("/")
+public class TestController extends BaseController {
+	
+	@RequestMapping("/test/page")
+	public Map<String, Object> test() {
+		return Map.of("id", "value");
+	}
+	
+	@RequestMapping("/test")
+	public String get() {
+		return "test";
+	}
+	
+}
